8b2afed8196a384b53a73f057b0e6285ccc16c90
Ryujeeee <tommy.ragot95@gmail.com>
2025-04-01 21:28:09 +0300

    Def function with IF ELSE

--- app.py
+++ app.py
@@ -28,3 +28,30 @@ surname = "Grey"
 student_name = name + " " + surname
 print(student_name)
 
+# TASK
+# 1. Write IF ELSE statement to validate if x is larger than y. Return TRUE if YES
+
+y = 5
+x = 10 
+if x > y:
+        print(True)  
+
+# With Function
+
+def eval (x, y):
+  if y > x:
+    return True
+    return False
+print(eval(300,200))
+
+# 2. Write IF ELSE statement to validate if x is larger than y and less than b. Return TRUE if YES
+
+def compareFunc2 (x, y, b):
+  if (y > x) and (x < b):
+    return True
+    return False
+
+print(compareFunc2(300,200,600))
+
+
+
