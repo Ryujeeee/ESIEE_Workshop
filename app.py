cbe4ae2f95a453ff2ffc2ffb118388cc65c94dd1
Ryujeeee <tommy.ragot95@gmail.com>
2025-04-01 21:04:31 +0300

    Created basis for our first application
    It includes mathematical operations and manipulation with data

---
+++ app.py
@@ -0,0 +1,30 @@
+# Simple Math
+
+x = 2
+y = 3
+z = x + y
+
+print(x)
+print(y)
+
+# Python is using indents
+
+a = 10
+b = 5 
+c = a / b
+d = a * b
+e = a % b
+
+a == b
+a || b
+a === b
+a != b
+a ^^ b
+a && b
+
+name = "Anna"
+surname = "Grey"
+
+student_name = name + " " + surname
+print(student_name)
+
